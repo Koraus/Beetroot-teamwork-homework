e961cbcdf7142fc44bbaa69bf19063e8b8e6803c
Moblat890 <moblat890@gmail.com>
2021-11-24 11:12:42 +0200

    rgp

--- JSstart/start.js
+++ JSstart/start.js
@@ -1,45 +1,45 @@
 //змінні
 {
-// var a;
-// let b;
-// const c;
+  // var a;
+  // let b;
+  // const c;
 }
 //типи
 {
-// console.log(typeof(1)); // число
-// console.log(typeof('1')); // строка
-// console.log(typeof(' ')); //  пробіл
-// console.log(typeof('')); //  пуста строка
-// console.log(typeof(true)); // булеве значення
-// console.log(typeof(false)); // булеве значення
+  // console.log(typeof(1)); // число
+  // console.log(typeof('1')); // строка
+  // console.log(typeof(' ')); //  пробіл
+  // console.log(typeof('')); //  пуста строка
+  // console.log(typeof(true)); // булеве значення
+  // console.log(typeof(false)); // булеве значення
 }
 
 {// Порівняння
-// == (перед тим як зрівняти значення - привде до одного типу і потім виконає ===) // 
-// === (порівняє типи данних і занчення) 
-// let a = 'Yes';
-// console.log( 1 ==  '1'); // 1 і строка 1
-// console.log( 1 ===  '1'); // 1 і строка 1
-// console.log( a ===  'Yes'); // 1 і строка 1
-// console.log( a ===  'yes'); // 1 і строка 1
-// console.log (typeof(Number(a)) + ' ' + a )
+  // == (перед тим як зрівняти значення - привде до одного типу і потім виконає ===) // 
+  // === (порівняє типи данних і занчення) 
+  // let a = 'Yes';
+  // console.log( 1 ==  '1'); // 1 і строка 1
+  // console.log( 1 ===  '1'); // 1 і строка 1
+  // console.log( a ===  'Yes'); // 1 і строка 1
+  // console.log( a ===  'yes'); // 1 і строка 1
+  // console.log (typeof(Number(a)) + ' ' + a )
 }
 {//Умови
 
-// const a = 'Yes'
-// if (a === "yes") {
-//     console.log('JS зайшов в тіло умови')
-// } else {  console.log('JS Не зайшов в тіло умови') };
-    
+  // const a = 'Yes'
+  // if (a === "yes") {
+  //     console.log('JS зайшов в тіло умови')
+  // } else {  console.log('JS Не зайшов в тіло умови') };
+
 }
 
 {// Цикли
-// for (let i = 1; true; i++){
-//     console.log('Я зараз на кроці ' + i);
-//     if (i === 20){
-//         break;
-//     }
-// }
+  // for (let i = 1; true; i++){
+  //     console.log('Я зараз на кроці ' + i);
+  //     if (i === 20){
+  //         break;
+  //     }
+  // }
 }
 
 //  { //Функції
@@ -131,11 +131,11 @@
 // яка приймає три окремі цифри і перетворює їх в одне число.
 // Наприклад: цифри 1, 4, 9 перетворяться в число 149.
 
-function uniteThreeNumbers(number1, number2, number3) {
+/* function uniteThreeNumbers(number1, number2, number3) {
         let result = "" + number1 + number2 + number3;
         return parseInt(result);
 
-   
+
 }
 console.log(uniteThreeNumbers(1, 4, 3));
-console.log(typeof(uniteThreeNumbers(1, 2, 3)));
+console.log(typeof(uniteThreeNumbers(1, 2, 3))); */
