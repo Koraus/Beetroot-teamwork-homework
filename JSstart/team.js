f7c46ad710f07773e98e80a4fd6920a684155e9b
Koraus <dmytro.kashchynets@gmail.com>
2021-11-25 10:54:47 +0200

    UPD

--- JSstart/team.js
+++ JSstart/team.js
@@ -190,13 +190,23 @@ console.log(add(1,4));*/
 // arr.push('a');
 // arr.push('b');
 // console.log(arr);
-let even = 0;
-let odd = 0;
-for ( let i = 10; i <19; i ++){
-  if( i % 2 == 0) {
-    even = even + 1; 
-  } else odd = odd + 1;
+// let even = 0;
+// let odd = 0;
+// for ( let i = 10; i <19; i ++){
+//   if( i % 2 == 0) {
+//     even = even + 1; 
+//   } else odd = odd + 1;
 
-}
-console.log(' Кількість парних чисел :' + even + '\n' +
-            ' Кількість не парних чисел :' + odd ) ;
+// }
+// console.log(' Кількість парних чисел :' + even + '\n' +
+//             ' Кількість не парних чисел :' + odd ) ;
+
+
+function add (a, b){
+   let c = a + b;
+   return 'парне';
+};
+
+let g = 'Введене число ' + add(10,15)  ; 
+
+console.log(g);
