ed2c6ca7491cefbac1927ac841dc7f6fb0374686
Bogdan Tsaruk <u.s.a.bogdan@gmail.com>
2021-11-26 10:40:10 +0200

    New

---
+++ JSstart/26-11-2021.js
@@ -0,0 +1,23 @@
+// let start = 8;
+// let end = 10;
+
+// function isSeven (num){
+//    if (num == 7) {
+//        return true;
+//    } else { 
+//        return false;
+//    }
+// }
+
+// function diapazon (num1, num2){
+//     for (i = num1; i <=num2; i++){
+//        if (isSeven(i)) {
+//            console.log('Seven is in diapazon');
+//        } else {
+//             console.log('Seven is not in diapazon');
+//        } 
+//     }
+// }
+
+// diapazon(start, end);
+//console.log(isSeven());
