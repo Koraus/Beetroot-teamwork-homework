4645e10c346a43bf543766e07317c4bd3e72cb07
Bogdan Tsaruk <u.s.a.bogdan@gmail.com>
2021-11-28 14:23:11 +0200

    Dont know

--- HW-JS/33-Class-Ex3.js
+++ HW-JS/33-Class-Ex3.js
@@ -1,12 +1,11 @@
 // Завдання 3
 
-// Реалізуй клас Employee, що описує працівника, і створи масив працівників банку.
+//   Реалізуй клас Employee, що описує працівника, і створи масив працівників банку.
 
-// Реалізуй клас EmpTable для генерації HTML-коду таблиці зі списком працівників банку.
-// Масив працівників необхідно передавати через конструктор, а отримувати HTML-код за допомогою методу getHtml ().
+//   Реалізуй клас EmpTable для генерації HTML-коду таблиці зі списком працівників банку.
+//  Масив працівників необхідно передавати через конструктор, а отримувати HTML-код за допомогою методу getHtml ().
 
-// Створи об'єкт класу EmpTable і виведи на екран результат роботи методу getHtml ().
-
+//  Створи об'єкт класу EmpTable і виведи на екран результат роботи методу getHtml ().
 
 
 class Employee  { 
@@ -18,24 +17,28 @@ class Employee  {
     set fullName(lastName ){
         [this._firstName, this._lastName] = lastName.split(' ');
 }
-    get fullName (){
+    get fullName2 (){
         return `${this._firstName} ${this._lastName}`;
     }
 
 };
 
 let a = new Employee ('Dmytro' , 'Yaroslavovych');
+
+console.log(a);
+
+a.fullName = 'Vasya Pupkin';
 console.log(a);
 
 
-function sss (a, f) {
-    let g = a;
-    let h = f;
-    let k = {
-        product : a,
-        kiltist : k,
-    }
-    return k;
-}
+// function sss (a, f) {
+//     let g = a;
+//     let h = f;
+//     let k = {
+//         product : a,
+//         kiltist : f,
+//     }
+//     return k;
+// }
 
-sss(19,)
+// console.log(sss(19,2));
