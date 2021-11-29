4913b0144d4da77cc7d195407b840a7ec638d2fe
Moblat890 <moblat890@gmail.com>
2021-11-29 11:42:10 +0200

    уен

--- HW-JS/33-Class-Ex3.js
+++ HW-JS/33-Class-Ex3.js
@@ -9,45 +9,45 @@
 //  Створи об'єкт класу EmpTable і виведи на екран результат роботи методу getHtml ().
 
 
-class Employee  {  // Реалізуй клас Employee, що описує працівника
-    constructor (firstName, lastName ) {
-        this._firstName = firstName;
-        this._lastName = lastName;
-    };
+class Employee {  // Реалізуй клас Employee, що описує працівника
+  constructor(firstName, lastName) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  };
 
-    set fullName(lastName ){
-        [this._firstName, this._lastName] = lastName.split(' ');
-}
-    get fullName2 (){
-        return `${this._firstName} ${this._lastName}`;
-    }
+  set fullName(lastName) {
+    [this._firstName, this._lastName] = lastName.split(' ');
+  }
+  get fullName2() {
+    return `${this._firstName} ${this._lastName}`;
+  }
 
 };
 
-let  employee1 = new Employee ('Dmytro' , 'Yaroslavovych');
-let  employee2 = new Employee ('Vasyl' , 'Ivanovych');
-let  employee3 = new Employee ('Oleg' , 'Viktorovych');
+let employee1 = new Employee('Dmytro', 'Yaroslavovych');
+let employee2 = new Employee('Vasyl', 'Ivanovych');
+let employee3 = new Employee('Oleg', 'Viktorovych');
 let arrEmployee = [employee1, employee2, employee3] //створи масив працівників банку.
 // console.log(arrEmployee);
 
 
 
 class EmpTable { // Реалізуй клас EmpTable
-    constructor(arrEmployee){ //Масив працівників необхідно передавати через конструктор
+  constructor(arrEmployee) { //Масив працівників необхідно передавати через конструктор
     this._arrEmployee = arrEmployee;
-    }
+  }
 
-    get getHtml (){ //генерації HTML-коду таблиці зі списком працівників банку.
-      let napovnenna = ``;
+  get getHtml() { //генерації HTML-коду таблиці зі списком працівників банку.
+    let napovnenna = ``;
 
-        for (let i = 0; i < this._arrEmployee.length; i++ ){          
-            napovnenna = napovnenna + `<tr> <td> ${i + 1} </td> <td> ${this._arrEmployee[i]._firstName} </td>  <td>${this._arrEmployee[i]._lastName} </td></tr>` ;
-        }
-        let info = `<table> ${napovnenna}}  </table>`
-        return info;
+    for (let i = 0; i < this._arrEmployee.length; i++) {
+      napovnenna = napovnenna + `<tr> <td> ${i + 1} </td> <td> ${this._arrEmployee[i]._firstName} </td>  <td>${this._arrEmployee[i]._lastName} </td></tr>`;
     }
+    let info = `<table> ${napovnenna}  </table>`
+    return info;
+  }
 
 }
 
-let table = new EmpTable (arrEmployee);
+let table = new EmpTable(arrEmployee);
 console.log(table.getHtml)//отримувати HTML-код за допомогою методу get
