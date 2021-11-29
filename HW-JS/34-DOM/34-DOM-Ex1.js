75cfe3d72b237e5c622d17d80d3f33cd652cb359
Koraus <dmytro.kashchynets@gmail.com>
2021-11-29 12:24:21 +0200

    UPD

---
+++ HW-JS/34-DOM/34-DOM-Ex1.js
@@ -0,0 +1,50 @@
+let playList = [
+    {
+        author: "LED ZEPPELIN",
+        song: "STAIRWAY TO HEAVEN"
+
+    },
+
+    {
+        author: "QUEEN",
+        song: "BOHEMIAN RHAPSODY"
+    },
+
+    {
+        author: "LYNYRD SKYNYRD",
+        song: "FREE BIRD"
+    },
+
+    {
+        author: "DEEP PURPLE",
+        song: "SMOKE ON THE WATER"
+    },
+
+    {
+        author: "JIMI HENDRIX",
+        song: "ALL ALONG THE WATCHTOWER"
+    },
+
+    {
+        author: "AC/DC",
+        song: "BACK IN BLACK"
+    },
+
+    {
+        author: "QUEEN",
+        song: "WE WILL ROCK YOU"
+    },
+
+    {
+        author: "METALLICA",
+        song: "ENTER SANDMAN"
+    }
+];
+
+let listForHtml = ``; 
+
+for ( i = 0; i < playList.length; i++ ){
+    listForHtml = listForHtml + ` </p> ${i+1} ${playList[i].author} ${playList[i].song} </p> `
+}
+
+document.write(listForHtml);
