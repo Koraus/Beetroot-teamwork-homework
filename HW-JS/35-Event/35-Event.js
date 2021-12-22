6fcf2c3dbfc51a2f7495f1f18249b48b3ae00088
Koraus <dmytro.kashchynets@gmail.com>
2021-12-22 20:35:08 +0200

    UPD

--- HW-JS/35-Event/35-Event.js
+++ HW-JS/35-Event/35-Event.js
@@ -1,3 +1,24 @@
-// Створити HTML-сторінку для відображення/редагування тексту. При відкритті сторінки текст відображається за допомогою тега div. При натисканні Ctrl + E, замість div з'являється textarea з тим же текстом, який тепер можна редагувати. При натисканні Ctrl +, замість textarea з'являється div з уже зміненим текстом. Не забудь вимкнути поведінку за замовчуванням для цих поєднань клавіш.
+// Створити HTML-сторінку для відображення/редагування тексту. При відкритті сторінки текст відображається за допомогою тега div. 
+// При натисканні Ctrl + E,
+//  замість div з'являється textarea з тим же текстом, який тепер можна редагувати.
 
-alert('Ok')
+
+// При натисканні Ctrl +, замість textarea з'являється div з уже зміненим текстом. 
+
+// Не забудь вимкнути поведінку за замовчуванням для цих поєднань клавіш.
+
+
+
+let textDiv = document.getElementById('text-fild');
+let textArea = document.getElementById('text-area');
+
+document.addEventListener('keydown', function(event) {
+    event.preventDefault()
+    if ( (event.code == 'KeyE' || event.code === 'Comma' ) && event.ctrlKey ) {
+
+        textDiv.classList.add('whid');
+
+        textArea.classList.remove('whid');
+    }
+   
+  });
