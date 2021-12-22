a0522fa8a9551c4e096aac23021650972af5fc21
Koraus <dmytro.kashchynets@gmail.com>
2021-12-22 20:04:01 +0200

    UPD

---
+++ fn/fn.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+function isSomeNumber(number, numberToCheck){
+    if (number === numberToCheck) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(isSomeNumber(1,10))
