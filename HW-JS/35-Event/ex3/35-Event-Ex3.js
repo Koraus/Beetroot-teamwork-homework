f3c1f98aa92c3f65dd88aa770262c679de17bdbb
Koraus <dmytro.kashchynets@gmail.com>
2021-12-23 17:58:32 +0200

    UPD

--- HW-JS/35-Event/ex3/35-Event-Ex3.js
+++ HW-JS/35-Event/ex3/35-Event-Ex3.js
@@ -9,30 +9,31 @@ let divForText = document.querySelector('.text-div');
 // });
 
 
-// mousedown
-// mousemove
-// mouseup
+// // mousedown
+// // mousemove
+// // mouseup
 
-let startX = 0;
+let startX;
 let startY = 0;
 let endX;
 let endY = 0;
 let startWidth = parseInt(window.getComputedStyle(document.getElementById('textDiv')).width);
 let startHeight = parseInt(window.getComputedStyle(document.getElementById('textDiv')).height);
 
+// let evenMouseD = clickArea.addEventListener('mousedown', a => a);
 
 
-clickArea.addEventListener('mousedown', function (event) {
-    event.stopPropagation()
-    forClick()
-});
+clickArea.addEventListener('mousedown', forClick);
 
 
-function forClick(params) {
-    document.addEventListener('mousedown', function (event) {
-        startX = event.clientX;
-        startY = event.clientY;
-    });
+function forClick(evenMouseD) {
+
+
+
+
+
+
+
 
     document.addEventListener('mouseup', function (event) {
         endX = event.clientX;
@@ -43,8 +44,81 @@ function forClick(params) {
         divForText.style.width = `${startWidth + widthDivForChange}px`;
         divForText.style.height = `${startHeight + heightDivForChange}px`;
 
+
+
+        console.log(`
+        startX ${startX}
+        endX  ${endX}
+        startY  ${startY}
+        endY ${endY}
+
+       Width for change: ${widthDivForChange} 
+       Height for change:  ${heightDivForChange}
+        ` )
     });
 
 }
 
 
+//функція яка при кліку записує кординати
+
+
+
+
+// function coordinatesMouthDowX() {
+//     return this.event.clientX;
+// }
+// function coordinatesMouthDowY() {
+//     return this.event.clientY;
+// }
+
+// function coordinatesMouthUpX() {
+//     return this.event.clientX;
+// }
+// function coordinatesMouthUpY() {
+//     return this.event.clientY;
+// }
+
+
+
+
+
+function coordinatesMouthX() {
+    return this.event.clientX;
+}
+function coordinatesMouthY() {
+    return this.event.clientY;
+}
+
+function coordinateDifference(start, end) {
+    return end - start ; 
+
+
+
+
+}
+
+
+
+coordinateDifference();
+
+// Відслідкувати клік  по квадратику
+//
+// Отримати координати натисканняМиші (coordinatesMouthX, coordinatesMouthY)
+// Записати отримані координати в змінні
+
+// Отримати координати відпусканняМиші (coordinatesMouthX, coordinatesMouthY)
+// Записати отримані координати в змінні
+
+// Отримати різницю координат X coordinateDifference
+// Отримати різницю координат Y coordinateDifference
+
+// Отримати значення поточної ширини div
+// Записати отримані координати в змінні
+
+// Отримати значення поточної висоти div
+// Записати отримані координати в змінні
+
+// зміна поточного значення ширини div
+
+// зміна поточного значення висоти div
---
+++ HW-JS/35-Event/ex3/refactEx35.js
@@ -0,0 +1,26 @@
+//Пошук елементів DOM і присвоєння їх в змінні
+let textDiv = document.getElementById('text-fild');
+let textArea = document.getElementById('text-area');
+
+// Відслідкувати клік  по квадратику  // event.list
+
+clickArea.addEventListener('mousedown', );
+
+// Отримати координати натисканняМиші (coordinatesMouthX, coordinatesMouthY)
+// Записати отримані координати в змінні
+
+// Отримати координати відпусканняМиші (coordinatesMouthX, coordinatesMouthY)
+// Записати отримані координати в змінні
+
+// Отримати різницю координат X coordinateDifference
+// Отримати різницю координат Y coordinateDifference
+
+// Отримати значення поточної ширини div
+// Записати отримані координати в змінні
+
+// Отримати значення поточної висоти div
+// Записати отримані координати в змінні
+
+// зміна поточного значення ширини div
+
+// зміна поточного значення висоти div
